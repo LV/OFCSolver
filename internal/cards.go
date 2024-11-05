1fc2b6b96597643a97f349c87f42d42c11fb4982
Luis Victoria <v@lambda.lv>
2024-11-04 21:10:16 -0500

    Create `Suit` and `Rank` enums, create `Symbols` rune

---
+++ internal/cards.go
@@ -0,0 +1,38 @@
+package cards
+
+import "fmt"
+
+type Suit int
+const (
+	Spades Suit = iota
+	Hearts
+	Diamonds
+	Clubs
+)
+
+type Rank int
+const (
+	Two Rank = iota
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+	Ace
+)
+
+var CardSymbols = []rune{
+	'🂡', '🂢', '🂣', '🂤', '🂥', '🂦', '🂧', '🂨', '🂩', '🂪', '🂫', '🂭', '🂮', // Spades
+	'🂱', '🂲', '🂳', '🂴', '🂵', '🂶', '🂷', '🂸', '🂹', '🂺', '🂻', '🂽', '🂾', // Hearts
+	'🃁', '🃂', '🃃', '🃄', '🃅', '🃆', '🃇', '🃈', '🃉', '🃊', '🃋', '🃍', '🃎', // Diamonds
+	'🃑', '🃒', '🃓', '🃔', '🃕', '🃖', '🃗', '🃘', '🃙', '🃚', '🃛', '🃝', '🃞', // Clubs
+	'🃏', '🃏',                                                      // Jokers
+}
+
+j
